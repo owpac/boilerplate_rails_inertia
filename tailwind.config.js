7018238ccd57ec46fba7d2a34af9427971e1046c
owpac <thomas.faugier@gmail.com>
2024-06-12 12:19:39 +0200

    fix(tailwind): fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.{js,jsx}'
+    './app/javascript/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
